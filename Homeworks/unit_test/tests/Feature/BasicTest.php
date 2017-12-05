018503bf2f80b489c6aefb09b2c776c6dd0aa255
Sunh <huusu1996@gmail.com>
2017-12-05 14:32:23 +0700

    finish

---
+++ Homeworks/unit_test/tests/Feature/BasicTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testHomeNotAuth()
+    {
+        $response = $this->get('/home');
+        $response->assertStatus(302);
+    }
+
+    public function testLogin()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+
+    public function testRegister()
+    {
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+
+    public function testResetPasss()
+    {
+        $response = $this->get('/password/reset');
+        $response->assertStatus(200);
+    }
+
+    public function testRegisterSuccess()
+    {
+        $response = $this->post('/register', [
+            'name' => 'demoname',
+            'email' => 'demoemail@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ]);
+        $response->assertStatus(500);
+    }
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
